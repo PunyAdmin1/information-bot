dadd492af2b55ab7a3825c8e1c6526a0de35f443
PunyAdmin1 <46908801+PunyAdmin1@users.noreply.github.com>
2020-03-02 19:13:43 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.on("ready", async () => {
+    console.log(`INDEX.JS | ${bot.user.username} has logged in.`)
+    bot.user.setActivity(`${bot.user.username} | ${bot.guilds.size} servers`, {type: "WATCHING"});
+})
+
+bot.on("message", (message) => {
+    var prefix = "!";
+
+    var msg = message.content.toLowerCase();
+
+    if (message.author.bot) return;
+
+    var mention = message.mentions.users.first();
+    var file1_1 = new Discord.Attachment("./Files/EmergencySet.rbxm");
+
+    if(msg.startsWith(prefix + "info1")) {
+        if(!message.member.roles.find(r => message.member.id === 474239530489085954)) return message.channel.sendMessage("Seriosly dude atleast you tried good sport :) but this command is only for Sean")
+        if (mention == null) { return; }
+        message.delete();
+        var file1 = new Discord.RichEmbed()
+        .setTitle("Important Announcement")
+        .setDescription("Hello, my name is <@474239530489085954> I am going to take a 1 day break off do not bother me all DM,s are closed until tommorow.")
+        .setColor(0xffffff);
+    }
+})
+
+bot.login("NjgzNzA1NTg3MDQwNzgwMzUx.XlvceA.1Sy1C0QKUa0wltP9aXeDMdRkkU0");
